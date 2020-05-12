6101cd62128f252e9529327968a54ab8e9a86fae
Alastair Smith <github@alastairsmith.me.uk>
2020-05-12 08:22:23 +0100

    dotnet new Provider solution

---
+++ provider/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Sample.Provider.Pacts.csproj
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}

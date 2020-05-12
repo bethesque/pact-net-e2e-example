86384a0a33dfb8ffc45ee73b05d2b672ec69c154
Alastair Smith <github@alastairsmith.me.uk>
2020-05-12 22:12:09 +0100

    Strip the web app right down to the bare bones
    We only need endpoint routing for this sample.

--- provider/test/Support/TestStartup.cs
+++ provider/test/Support/TestStartup.cs
@@ -1,15 +1,14 @@
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 
 namespace Sample.Provider.Pacts.Support
 {
     internal class TestStartup
     {
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
             var apiStartup = new Startup();
             app.UseMiddleware<ProviderStateMiddleware>();
-            apiStartup.Configure(app, env);
+            apiStartup.Configure(app);
         }
     }
 }
